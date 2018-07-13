093d7f8140fa61771ce4ca15743331597868e1b9
JuanMa Garrido <juanma.garrido@gmail.com>
2018-07-13 10:09:16 +0200

    scss support

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
-/* import AtomTooltip from '@s-ui/react-atom-tooltip' */
+import AtomTooltip from '@s-ui/react-atom-tooltip'
+
+import "@s-ui/react-atom-tooltip/lib/index.scss"
 
 const App = () => (
   <Fragment>
     <p>Welcome to this demo</p>
-    {/* <p>Welcome to <AtomTooltip><strong title="Simplifying SSR w/ react">next.js!</strong></AtomTooltip></p> */}
+    <p>Welcome to <AtomTooltip><strong title="Simplifying SSR w/ react">next.js!</strong></AtomTooltip></p>
   </Fragment>
 )
 
